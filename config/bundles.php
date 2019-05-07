266db1597040987c59ed8390a34351af523bca8e
Yevhen Minenko <devyevhen1990@gmail.com>
2019-05-07 10:54:05 +0300

    Install dev bundle - server.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
